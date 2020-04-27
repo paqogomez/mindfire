8906428ab9bfa2eae94be8e95c3c7f885590bb31
Chad Thompson <chad.thompson@extraspace.com>
2020-04-27 17:23:46 -0600

    removed position component

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import Earthquakes from './components/earthquakes';
+
 
 
 test('should have title of earthquakes close to you', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Earthquakes close to you./i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-test('should have title of earthquakes close to you', () => {
-  const { getByText } = render(<Earthquakes />);
   const linkElement = getByText(/Earthquakes close to you./i);
   expect(linkElement).toBeInTheDocument();
 });
